b5750b408c729c6140fda2db6ba1fda703218548
RBucheli8 <141966641+RBucheli8@users.noreply.github.com>
2023-10-02 01:53:37 -0500

    Corrección constructores

--- src/main/java/vehiculos/Camion.java
+++ src/main/java/vehiculos/Camion.java
@@ -4,8 +4,7 @@ public class Camion extends Vehiculo {
 	private int ejes;
 	private static int cantidadCamiones;
 	
-	public Camion(String placa, int puertas, int velocidadMaxima, String nombre, int precio, 
-				int peso, String traccion, Fabricante fabricante, int ejes) {
+	public Camion(String placa, String nombre, int precio, int peso, Fabricante fabricante, int ejes) {
 		super(placa, 2, 80, nombre, precio, peso, "4X2", fabricante);
 		this.ejes = ejes;
 	}
--- src/main/java/vehiculos/Camioneta.java
+++ src/main/java/vehiculos/Camioneta.java
@@ -4,8 +4,8 @@ public class Camioneta extends Vehiculo {
 	private boolean volco;
 	private static int cantidadCamionetas;
 		
-	public Camioneta(String placa, int puertas, int velocidadMaxima, String nombre, int precio, 
-				int peso, String traccion, Fabricante fabricante, boolean volco) {
+	public Camioneta(String placa, int puertas, String nombre, int precio, 
+				int peso, Fabricante fabricante, boolean volco) {
 		super(placa, puertas, 90, nombre, precio, peso, "4X4", fabricante);
 		this.volco = volco;
 	}
